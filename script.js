c935384bb989727b9cc42259b0df59cd07cffa52
Daniel <roderm99@gmail.com>
2024-01-04 13:53:42 -0500

    create 'game' function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let winRound = null;
+
 function getComputerChoice() {
 
         let options = ["Rock", "Paper", "Scissors"];
@@ -19,7 +21,8 @@ function capitalize(string){
 }
 
 function playRound(playerSelection, computerSelection) {
-
+    
+    playerSelection = prompt("Choose Your Weapon.");
     let player = capitalize(playerSelection);
     computerSelection = getComputerChoice();
 
@@ -29,30 +32,73 @@ function playRound(playerSelection, computerSelection) {
     let badChoice = "Choose 'Rock', 'Paper' or 'Scissors' idiot. Not that hard";
 
     if (player == "Rock" && computerSelection == "Scissors") {
+        winRound = true;
         return winAlert;
     } else if (player == "Rock" && computerSelection == "Paper") {
+        winRound = false;
         return loseAlert;
     } else if (player == "Rock" && computerSelection == "Rock") {
+        winRound = null;
         return tieAlert;
+        // console.log(tieAlert);
+        // playRound(playerSelection, computerSelection);
     } else if (player == "Paper" && computerSelection == "Scissors") {
+        winRound = false;
         return loseAlert;
     } else if (player == "Paper" && computerSelection == "Rock") {
+        winRound = true;
         return winAlert;
     }else if (player == "Paper" && computerSelection == "Paper") {
+        winRound = null;
         return tieAlert;
+        // console.log(tieAlert);
+        // playRound(playerSelection, computerSelection);
     } else if (player == "Scissors" && computerSelection == "Scissors") {
-       return tieAlert;
+        winRound = null;
+        return tieAlert;
+        // console.log(tieAlert);
+        // playRound(playerSelection, computerSelection);
     }else if (player == "Scissors" && computerSelection == "Paper") {
+        winRound = true;
         return winAlert;
     } else if (player == "Scissors" && computerSelection == "Rock") {
+        winRound = false;
         return loseAlert;
     } else {
-
+        winRound = null;
         return badChoice;
     }
 
 }
 
 function game() {
-    
+    console.log("Starting Game");
+
+    let playerCount = 0;
+    let computerCount = 0;
+    let winGame = "GAME WINNER";
+    let loseGame = "GAME LOSER";
+
+    for (gameCount = 0; gameCount < 5;) {
+        winRound = null;
+        gameCount++;
+        console.log(playRound());
+
+        if (winRound == true) {
+            playerCount++;
+        } else if (winRound == false) {
+            computerCount++;
+        } else {
+            --gameCount;
+        }
+
+        if (playerCount == 3) {
+            console.log(winGame);
+        }
+
+        if (computerCount == 3) {
+            console.log(loseGame);
+        }
+    }
+
 }
